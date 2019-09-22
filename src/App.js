af8edb16faab2934a8019a4cd91a309641218961
jinimcoroneo <jinim1983@gmail.com>
2019-09-23 00:48:48 +0100

    display weather details in weather component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React from 'react';
 import Title from './components/Title'
 import Form from './components/Form'
 import Weather from './components/Weather'
+import { throwStatement } from '@babel/types';
 
 const API_KEY = "a1f67c78a69309a76670e182397f506c";
 
 class App extends React.Component {
+  state = {
+    temperature: "",
+    humidity: "",
+    pressure: "",
+    time: ""
+  }
   getLondonWeather = async () => {
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${API_KEY}&units=metric`)
     const data = await api_call.json()
-    console.log(data);
+    this.setState({
+      time: new Date().toLocaleString(),
+      temperature: data.main.temp,
+      humidity: data.main.humidity,
+      pressure: data.main.pressure,
+    })
   };
 
   render() {
@@ -17,7 +29,12 @@ class App extends React.Component {
       <div>
         <Title />
         <Form getLondonWeather={this.getLondonWeather}/>
-        <Weather />
+        <Weather 
+          time={this.state.time}
+          temperature={this.state.temperature}
+          humidity={this.state.humidity}
+          pressure={this.state.pressure}
+        />
       </div>
     );
   }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,7 +3,11 @@ import React from 'react';
 class Weather extends React.Component {
     render() {
         return (
-            <div>Weather Data</div>
+            <div>
+                <p>{this.props.time}</p>
+                <p>{this.props.temperature}</p>
+                <p>{this.props.humidity}% Humidity</p>
+            </div>
         );
     }
 };
